9c285f322bd1df021b76cf589d47c265d032e706
pizzamoeger <osshannahsophie@gmail.com>
2025-05-04 17:05:21 +0200

    [Timetable] renamed dashboard to timetable

--- app/src/main/java/com/example/studentapp/ui/timetable/TimetableFragment.kt
+++ app/src/main/java/com/example/studentapp/ui/timetable/TimetableFragment.kt
@@ -1,4 +1,4 @@
-package com.example.studentapp.ui.dashboard
+package com.example.studentapp.ui.timetable
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.studentapp.databinding.FragmentDashboardBinding
+import com.example.studentapp.databinding.FragmentTimetableBinding
 
-class DashboardFragment : Fragment() {
+class TimetableFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentTimetableBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+            ViewModelProvider(this).get(TimetableViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentTimetableBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textTimetable
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }

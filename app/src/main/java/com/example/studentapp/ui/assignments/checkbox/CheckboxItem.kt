ed1afe77e62e0ccee0b0a055a89e574bb744af26
pizzamoeger <osshannahsophie@gmail.com>
2025-05-19 07:59:47 +0200

    Revert "[assignment] UI for subtasks (WIP) merge"
    This reverts commit 1af3633b3b175f5783496a598f16d0af9eef375d.

--- app/src/main/java/com/example/studentapp/ui/assignments/checkbox/CheckboxItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.studentapp.ui.assignments.checkbox
-
-sealed class CheckboxItem {
-    data class Checkbox(val text: String, var checked: Boolean = false) : CheckboxItem()
-    object InputBox : CheckboxItem()
-}

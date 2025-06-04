158c7baea3428bad68566903ad5ded78c2feca25
pizzamoeger <osshannahsophie@gmail.com>
2025-06-04 18:35:06 +0200

    [ects] it looks a bit better now

--- app/src/main/java/com/hannah/studentapp/ui/type/TypeAdapter.kt
+++ app/src/main/java/com/hannah/studentapp/ui/type/TypeAdapter.kt
@@ -1,10 +1,15 @@
 package com.hannah.studentapp.ui.type
 
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.hannah.studentapp.R
 import com.hannah.studentapp.databinding.AssignmentBinding
 import com.hannah.studentapp.databinding.EctsTypeItemBinding
+import com.hannah.studentapp.ui.getThemeColor
 
 class TypeAdapter (): RecyclerView.Adapter<TypeAdapter.TypeViewHolder> () {
     inner class TypeViewHolder(
@@ -14,6 +19,12 @@ class TypeAdapter (): RecyclerView.Adapter<TypeAdapter.TypeViewHolder> () {
             binding.ectsTypeItemName.text = item.getName()
             binding.ectsTypeItemPending.text = item.getOngoingECTS().toString()
             binding.ectsTypeItemNeeded.text = (item.getECTSNeeded()-item.getCompletedECTS()).toString()
+            if (item.getECTSNeeded()-item.getCompletedECTS() < 0) {
+                val context = binding.root.context
+                val baseColor = ContextCompat.getColor(context, R.color.green)
+                val transparentColor = Color.argb(100, Color.red(baseColor), Color.green(baseColor), Color.blue(baseColor))
+                binding.root.setBackgroundColor(transparentColor)
+            }
             //binding.ectsTypeItemPassed.text = item.getCompletedECTS().toString()
         }
     }

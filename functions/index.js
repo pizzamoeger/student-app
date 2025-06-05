dc4c9c6a507430f307128b353a2245b476a626e1
pizzamoeger <osshannahsophie@gmail.com>
2025-06-05 23:39:21 +0200

    Revert "Revert "[notis] still trying to set it up""
    This reverts commit d982f5de287d71a30ea3e574eaca353bd8967d6e.

--- functions/index.js
+++ functions/index.js
@@ -1,70 +1,38 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-const {onSchedule} = require("firebase-functions/v2/pubsub");
+const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.sendScheduledNotifications = onSchedule("every 1 minutes",
-    async (event) => {
-      const now = Date.now();
+exports.sendScheduledNotification = functions.pubsub
+    .schedule("every day 08:00")
+    .timeZone("Europe/Berlin")
+    .onRun(async (context) => {
+      const db = admin.firestore();
 
-      const notificationsRef = admin.firestore().collection("notifications");
-      const snapshot = await notificationsRef.where("scheduledTime", "<=", now)
-          .where("sent", "==", false).get();
+      try {
+        const usersSnapshot = await db.collection("users").get();
+        const tokens = [];
 
-      if (snapshot.empty) {
-        console.log("No notifications to send");
-        return null;
-      }
+        usersSnapshot.forEach((doc) => {
+          const data = doc.data();
+          if (data.fcmToken) {
+            tokens.push(data.fcmToken);
+          }
+        });
 
-      const batch = admin.firestore().batch();
-
-      const sendPromises = [];
-
-      snapshot.forEach((doc) => {
-        const data = doc.data();
         const message = {
-          token: data.fcmToken,
           notification: {
-            title: data.title,
-            body: data.body,
-          },
-          android: {
-            notification: {
-              channelId: "scheduled_notifications",
-              sound: "default",
-            },
+            title: "Good Morning!",
+            body: "Don't forget to check your timetable for today.",
           },
         };
 
-        sendPromises.push(
-            admin.messaging().send(message).then(() => {
-              console.log(`Notification sent to token ${data.fcmToken}`);
-              batch.update(doc.ref, {sent: true});
-            }).catch((err) => {
-              console.error("Error sending notification:", err);
-            }),
+        const sendPromises = tokens.map((token) =>
+          admin.messaging().send({...message, token}),
         );
-      });
 
-      await Promise.all(sendPromises);
-      return batch.commit();
+        await Promise.all(sendPromises);
+        console.log("Notifications sent!");
+      } catch (error) {
+        console.error("Error sending notifications:", error);
+      }
     });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
